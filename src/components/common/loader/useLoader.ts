f7ae0dee479527785d3fe8783817c42c2d17e636
Nacho Mandagaran <33981565+ignmandagaran@users.noreply.github.com>
2022-06-01 16:00:13 -0300

    Add sculpture gallery example (#4)
    * add sculpture gallery example
    
    * add vr button
    
    * fix VRButton import
    
    * add VR support conditionally
    
    * fix navigator support condition
    
    * Revert "fix navigator support condition"
    
    This reverts commit 74a9a7bba87c6719a60aa76df96dd2811e7d57d3.
    
    * loader
    
    * addapting some code to createWorld helper
    
    Co-authored-by: Matias Perez <matiasperezpc@hotmail.com>

---
+++ src/components/common/loader/useLoader.ts
@@ -0,0 +1,11 @@
+import create from 'zustand'
+
+export const useLoader = create<{
+  loading: boolean
+  setLoading: () => void
+  setLoaded: () => void
+}>((set) => ({
+  loading: true,
+  setLoading: () => set((state) => ({ ...state, loading: true })),
+  setLoaded: () => set((state) => ({ ...state, loading: false }))
+}))
