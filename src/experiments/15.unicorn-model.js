e90a4a0122bdba28f08bf2c428b8d04e2109a3d2
Matias Perez <matiasperezpc@hotmail.com>
2022-06-07 13:15:44 -0300

    update unicorn

--- src/experiments/15.unicorn-model.js
+++ src/experiments/15.unicorn-model.js
@@ -1,58 +1,21 @@
-import { Center, Environment, OrbitControls, useGLTF } from '@react-three/drei'
-import { useRef } from 'react'
+import { Environment } from '@react-three/drei'
 
-const Unicorn = (props) => {
-  const group = useRef()
-  const { nodes, materials } = useGLTF('/models/Unicorn_v3.gltf')
+import { model } from '~/lib/builders'
+
+const Model = model('Unicorn_v3.gltf', {
+  scale: 1.4,
+  ambientLight: 0.1
+})
+
+const JustAModel = () => {
   return (
     <>
-      <OrbitControls />
       <Environment preset="sunset" />
-      {/* <pointLight position="" /> */}
-      <color attach="background" args={['#666']} />
-      <ambientLight intensity={0.1} />
-
-      <Center>
-        <group ref={group} {...props} dispose={null}>
-          <group position={[0, 0, 0.05]}>
-            <primitive object={nodes.spine001} />
-            <skinnedMesh
-              geometry={nodes.Horn.geometry}
-              material={materials['Material.003']}
-              skeleton={nodes.Horn.skeleton}
-            />
-            <skinnedMesh
-              geometry={nodes.Cola2.geometry}
-              material={nodes.Cola2.material}
-              skeleton={nodes.Cola2.skeleton}
-            />
-            <skinnedMesh
-              geometry={nodes.Hair.geometry}
-              material={nodes.Hair.material}
-              skeleton={nodes.Hair.skeleton}
-            />
-            <skinnedMesh
-              geometry={nodes.Body_1.geometry}
-              material={materials.Unicorn}
-              skeleton={nodes.Body_1.skeleton}
-            />
-            <skinnedMesh
-              geometry={nodes.Body_2.geometry}
-              material={materials.Black}
-              skeleton={nodes.Body_2.skeleton}
-            />
-            <skinnedMesh
-              geometry={nodes.Body_3.geometry}
-              material={nodes.Body_3.material}
-              skeleton={nodes.Body_3.skeleton}
-            />
-          </group>
-        </group>
-      </Center>
+      <Model />
     </>
   )
 }
 
-Unicorn.Title = 'This is an Unicorn'
+JustAModel.Title = 'This is an Unicorn'
 
-export default Unicorn
+export default JustAModel
--- src/lib/builders.tsx
+++ src/lib/builders.tsx
@@ -1,31 +1,40 @@
-import { Clone, OrbitControls, useGLTF } from '@react-three/drei'
+import { Center, OrbitControls, useGLTF } from '@react-three/drei'
 import { useControls } from 'leva'
 import { FC } from 'react'
 
 const baseControls = {
-  scale: 1
+  scale: 1,
+  ambientLight: 1
 }
 
 export const model = (path: string, config?: typeof baseControls): FC => {
   return () => {
     const model = useGLTF(`/models/${path}`)
-    const { scale } = useControls({
+    const resolvedConfig = useControls({
       scale: {
         min: 0,
         step: 0.1,
         value: config?.scale || baseControls.scale,
         max: 10
+      },
+      ambientLight: {
+        min: 0,
+        step: 0.1,
+        value: config?.ambientLight || baseControls.ambientLight,
+        max: 2
       }
     })
 
     return (
       <>
-        <ambientLight />
+        <ambientLight intensity={resolvedConfig?.ambientLight} />
         <OrbitControls />
-        <group scale={scale}>
-          {/*@ts-ignore*/}
-          <Clone object={model.scene} scale={scale} />
-        </group>
+        <Center>
+          <group scale={resolvedConfig?.scale}>
+            {/*@ts-ignore*/}
+            <primitive object={model.scene} />
+          </group>
+        </Center>
       </>
     )
   }
