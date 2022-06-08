e5adfa87b2eba48d178bc544bdf43f26e2c02d7b
Juan Lautaro Martin <lautaro@basement.studio>
2022-06-08 00:51:42 -0300

    FIX nextJS

--- src/experiments/17.next.js
+++ src/experiments/17.next.js
@@ -1,13 +1,13 @@
 import { model } from '~/lib/builders/model'
 
-const Unicorn = model('next.glb', {
+const nextJS = model('next.glb', {
   scale: 1.4,
   ambientLight: 0.1,
   background: '#000',
   environment: 'sunset'
 })
 
-Unicorn.Title = 'Next.js 3d model'
-Unicorn.Tags = '3d'
+nextJS.Title = 'Next.js 3d model'
+nextJS.Tags = '3d'
 
-export default Unicorn
+export default nextJS
