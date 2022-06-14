571ccf8f3817c7f05558cdd15f5757186e8208c6
Juan Lautaro Martin <lautaro@basement.studio>
2022-06-14 20:51:48 -0300

    Box test

---
+++ src/experiments/19.Box.js
@@ -0,0 +1,16 @@
+import { Loader } from '~/components/common/loader'
+import { R3FCanvasLayout } from '~/components/layout/r3f-canvas-layout'
+import { model } from '~/lib/builders/model'
+
+const BOX = model('box.glb', {
+  scale: 0.6,
+  ambientLight: 0.1,
+  background: '#000',
+  environment: 'sunset'
+})
+
+BOX.Layout = (props) => <R3FCanvasLayout {...props} htmlChildren={<Loader />} />
+BOX.Title = 'Hologram using Normals and HDRI Test'
+BOX.Tags = '3d'
+
+export default BOX
