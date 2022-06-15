7a1c6b4d21f8ba942305620ee01b7699c6e039ae
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2022-06-15 18:40:01 -0300

    add try catch

--- src/pages/api/experiments.ts
+++ src/pages/api/experiments.ts
@@ -1,5 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 
+import { internalServerError, success } from '~/lib/api-responses'
+
 import { getFileContributors } from '../../lib/github'
 import { getAllExperimentSlugs, getExamplePath } from '../../lib/utils'
 
@@ -7,51 +9,57 @@ export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const allSlugs = await getAllExperimentSlugs()
+  try {
+    const allSlugs = await getAllExperimentSlugs()
 
-  const modules = await Promise.all(
-    allSlugs.map((slug) =>
-      import(`~/experiments/${slug}`).then((m) => [slug, m.default])
-    )
-  )
-
-  let experiments = modules
-    .map((exp) => {
-      const title: string = exp[1].Title || exp[0]
-
-      return {
-        filename: exp[0],
-        title,
-        href: `/experiments/${exp[0]}`,
-        tags:
-          (exp[1].Tags as string)
-            ?.split(',')
-            ?.map((tag) => tag.toLowerCase().trim()) || []
-      }
-    })
-    .sort((a, b) =>
-      a.filename.localeCompare(b.filename, undefined, { numeric: true })
+    const modules = await Promise.all(
+      allSlugs.map((slug) =>
+        import(`~/experiments/${slug}`).then((m) => [slug, m.default])
+      )
     )
 
-  experiments = experiments.filter((e) => !e.tags.includes('private'))
+    let experiments = modules
+      .map((exp) => {
+        const title: string = exp[1].Title || exp[0]
 
-  // Numerate experiments
-  experiments = experiments.map((e, i) => ({
-    ...e,
-    number: i + 1
-  }))
+        return {
+          filename: exp[0],
+          title,
+          href: `/experiments/${exp[0]}`,
+          tags:
+            (exp[1].Tags as string)
+              ?.split(',')
+              ?.map((tag) => tag.toLowerCase().trim()) || []
+        }
+      })
+      .sort((a, b) =>
+        a.filename.localeCompare(b.filename, undefined, { numeric: true })
+      )
 
-  // Add contributors
-  experiments = await Promise.all(
-    experiments.map(async (e) => {
-      const contributors = await getFileContributors(getExamplePath(e.filename))
+    experiments = experiments.filter((e) => !e.tags.includes('private'))
 
-      return {
-        ...e,
-        contributors
-      }
-    })
-  )
+    // Numerate experiments
+    experiments = experiments.map((e, i) => ({
+      ...e,
+      number: i + 1
+    }))
 
-  res.status(200).json(experiments)
+    // Add contributors
+    experiments = await Promise.all(
+      experiments.map(async (e) => {
+        const contributors = await getFileContributors(
+          getExamplePath(e.filename)
+        )
+
+        return {
+          ...e,
+          contributors
+        }
+      })
+    )
+
+    success(res, experiments as any)
+  } catch (error) {
+    internalServerError(res, error)
+  }
 }
