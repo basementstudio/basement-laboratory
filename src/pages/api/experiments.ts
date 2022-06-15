30e5bee9b04ea2efc1b4cb0006a08b999aa5305b
Nacho Mandagaran <ignaciofmandagaran@gmail.com>
2022-06-15 18:12:10 -0300

    Revert "add try catch"
    This reverts commit 124964bc9de173d09c17fcef949dd89bf17e2b90.

--- src/pages/api/experiments.ts
+++ src/pages/api/experiments.ts
@@ -7,58 +7,51 @@ export default async function handler(
   _req: NextApiRequest,
   res: NextApiResponse
 ) {
-  try {
-    const allSlugs = await getAllExperimentSlugs()
+  const allSlugs = await getAllExperimentSlugs()
 
-    const modules = await Promise.all(
-      allSlugs.map((slug) =>
-        import(`~/experiments/${slug}`).then((m) => [slug, m.default])
-      )
+  const modules = await Promise.all(
+    allSlugs.map((slug) =>
+      import(`~/experiments/${slug}`).then((m) => [slug, m.default])
+    )
+  )
+
+  let experiments = modules
+    .map((exp) => {
+      const title: string = exp[1].Title || exp[0]
+
+      return {
+        filename: exp[0],
+        title,
+        href: `/experiments/${exp[0]}`,
+        tags:
+          (exp[1].Tags as string)
+            ?.split(',')
+            ?.map((tag) => tag.toLowerCase().trim()) || []
+      }
+    })
+    .sort((a, b) =>
+      a.filename.localeCompare(b.filename, undefined, { numeric: true })
     )
 
-    let experiments = modules
-      .map((exp) => {
-        const title: string = exp[1].Title || exp[0]
+  experiments = experiments.filter((e) => !e.tags.includes('private'))
 
-        return {
-          filename: exp[0],
-          title,
-          href: `/experiments/${exp[0]}`,
-          tags:
-            (exp[1].Tags as string)
-              ?.split(',')
-              ?.map((tag) => tag.toLowerCase().trim()) || []
-        }
-      })
-      .sort((a, b) =>
-        a.filename.localeCompare(b.filename, undefined, { numeric: true })
-      )
+  // Numerate experiments
+  experiments = experiments.map((e, i) => ({
+    ...e,
+    number: i + 1
+  }))
 
-    experiments = experiments.filter((e) => !e.tags.includes('private'))
+  // Add contributors
+  experiments = await Promise.all(
+    experiments.map(async (e) => {
+      const contributors = await getFileContributors(getExamplePath(e.filename))
 
-    // Numerate experiments
-    experiments = experiments.map((e, i) => ({
-      ...e,
-      number: i + 1
-    }))
+      return {
+        ...e,
+        contributors
+      }
+    })
+  )
 
-    // Add contributors
-    experiments = await Promise.all(
-      experiments.map(async (e) => {
-        const contributors = await getFileContributors(
-          getExamplePath(e.filename)
-        )
-
-        return {
-          ...e,
-          contributors
-        }
-      })
-    )
-
-    res.status(200).json(experiments)
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ error: err.message })
-  }
+  res.status(200).json(experiments)
 }
