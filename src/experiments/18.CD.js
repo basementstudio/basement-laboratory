ed8292439c65ecbc892c9bcf3ee2e8d1335efd3f
Matias Perez <matiasperezpc@hotmail.com>
2022-06-19 15:34:39 -0300

    update

--- src/experiments/18.CD.js
+++ src/experiments/18.CD.js
@@ -1,14 +1,11 @@
 import { model } from '~/lib/builders/model'
 
-const CD = model(
-  { path: 'cd.glb', environment: 'test.hdri.1.hdr' },
-  {
-    scale: 0.6,
-    ambientLight: 0.1,
-    background: '#000',
-    environment: 'sunset'
-  }
-)
+const CD = model('cd.glb', {
+  scale: 0.6,
+  ambientLight: 0.1,
+  background: '#000',
+  environment: 'sunset'
+})
 
 CD.Title = 'CD founded under the desk'
 CD.Tags = '3d'
