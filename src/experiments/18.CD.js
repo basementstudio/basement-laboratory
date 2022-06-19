4f8db6f968ca561da44eb056b3e6e68b93ac7b90
Matias Perez <matiasperezpc@hotmail.com>
2022-06-19 15:20:20 -0300

    update model func

--- src/experiments/18.CD.js
+++ src/experiments/18.CD.js
@@ -1,15 +1,15 @@
-import { Loader } from '~/components/common/loader'
-import { R3FCanvasLayout } from '~/components/layout/r3f-canvas-layout'
 import { model } from '~/lib/builders/model'
 
-const CD = model('cd.glb', {
-  scale: 0.7,
-  ambientLight: 0.1,
-  background: '#000',
-  environment: 'sunset'
-})
+const CD = model(
+  { path: 'cd.glb', environment: 'test.hdri.1.hdr' },
+  {
+    scale: 0.7,
+    ambientLight: 0.1,
+    background: '#000',
+    environment: 'sunset'
+  }
+)
 
-CD.Layout = (props) => <R3FCanvasLayout {...props} htmlChildren={<Loader />} />
 CD.Title = 'CD founded under the desk'
 CD.Tags = '3d'
 
--- src/lib/builders/model.tsx
+++ src/lib/builders/model.tsx
@@ -4,7 +4,8 @@ import { presetsObj } from '@react-three/drei/helpers/environment-assets.cjs'
 import { useControls } from 'leva'
 import { FC } from 'react'
 
-import { useLoader } from '~/components/common/loader'
+import { Loader, useLoader } from '~/components/common/loader'
+import { R3FCanvasLayout } from '~/components/layout/r3f-canvas-layout'
 
 const baseControls = {
   scale: 1,
@@ -13,8 +14,14 @@ const baseControls = {
   environment: 'city'
 }
 
-export const model = (path: string, config?: typeof baseControls): FC => {
-  return () => {
+export const model = (
+  input: string | { path: string; environment: string },
+  controls?: typeof baseControls
+): FC => {
+  const { path, environment } =
+    typeof input === 'string' ? { path: input, environment: undefined } : input
+
+  const Comp = () => {
     const setLoaded = useLoader((s) => s.setLoaded)
     const model = useGLTF(`/models/${path}`, undefined, undefined, (loader) => {
       loader.manager.onLoad = () => setLoaded()
@@ -24,29 +31,40 @@ export const model = (path: string, config?: typeof baseControls): FC => {
       scale: {
         min: 0,
         step: 0.1,
-        value: config?.scale || baseControls.scale,
+        value: controls?.scale || baseControls.scale,
         max: 10
       },
       ambientLight: {
         min: 0,
         step: 0.1,
-        value: config?.ambientLight || baseControls.ambientLight,
+        value: controls?.ambientLight || baseControls.ambientLight,
         max: 2
       },
       background: {
-        value: config?.background || baseControls.background
+        value: controls?.background || baseControls.background
       },
-      environment: {
-        options: Object.keys(presetsObj),
-        value: config?.environment || baseControls.environment
-      }
+      environment: environment
+        ? {
+            value: environment,
+            disabled: true
+          }
+        : {
+            options: Object.keys(presetsObj),
+            value: controls?.environment || baseControls.environment
+          }
     })
 
     return (
       <>
         <color attach="background" args={[resolvedConfig.background]} />
         {/* @ts-ignore */}
-        <Environment preset={resolvedConfig?.environment} />
+        <Environment
+          {...(environment
+            ? {
+                files: `/environments/${environment}`
+              }
+            : { preset: resolvedConfig?.environment })}
+        />
         <ambientLight intensity={resolvedConfig?.ambientLight} />
         <OrbitControls />
         <Center>
@@ -58,4 +76,10 @@ export const model = (path: string, config?: typeof baseControls): FC => {
       </>
     )
   }
+
+  Comp.Layout = (props: any) => (
+    <R3FCanvasLayout {...props} htmlChildren={<Loader />} />
+  )
+
+  return Comp
 }
