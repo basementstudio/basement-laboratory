1f63fa955ffb44678dba342aed74a7b6fd22c50d
Matias Perez <matiasperezpc@hotmail.com>
2022-07-04 10:54:41 -0300

    api

--- src/components/common/welcome/experiments-section.tsx
+++ src/components/common/welcome/experiments-section.tsx
@@ -95,7 +95,6 @@ const ExperimentsSection: FC<ExperimentsSectionProps> = ({ experiments }) => {
                         <Link href={user.url}>
                           <a>
                             <Image
-                              // @ts-ignore
                               layout="raw"
                               width={32}
                               height={32}
