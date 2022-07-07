2b3e2dab909648540162002c43fff990996995b7
Juan Lautaro Martin <lautaro@basement.studio>
2022-07-07 15:42:45 -0300

    Cupcake v1

---
+++ src/experiments/23.cupcake.js
@@ -0,0 +1,16 @@
+import { Loader } from '~/components/common/loader'
+import { R3FCanvasLayout } from '~/components/layout/r3f-canvas-layout'
+import { model } from '~/lib/builders/model'
+
+const cupcake = model('cupcake.glb', {
+  scale: 0.6,
+  ambientLight: 0.1,
+  background: '#FFF',
+  environment: 'sunset'
+})
+
+cupcake.Layout = (props) => <R3FCanvasLayout {...props} htmlChildren={<Loader />} />
+cupcake.Title = 'Num Num Num 3D Cupcake'
+cupcake.Tags = '3d'
+
+export default cupcake
