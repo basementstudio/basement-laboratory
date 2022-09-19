77e40489747523c2ed415112508d43e2200f48c5
Matias Perez <matiasperezpc@hotmail.com>
2022-09-19 01:07:00 -0300

    update

--- src/experiments/27.reflective-effect.js
+++ src/experiments/27.reflective-effect.js
@@ -1,5 +1,5 @@
 import { useGsapFrame } from '@basementstudio/definitive-scroll/hooks'
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 import { SmoothScrollLayout } from '../components/layout/smooth-scroll-layout'
 import { useDeviceDetect } from '../hooks/use-device-detect'
@@ -32,39 +32,83 @@ const useMouse = () => {
 }
 
 const ReflectiveEffect = () => {
-  const [angle, setAngle] = useState(0)
+  const [orientation, setOrientation] = useState({
+    x: 0,
+    y: 0,
+    normalized: { x: 0, y: 0 }
+  })
   const { normalized } = useMouse()
   const { isDesktop } = useDeviceDetect()
   const { mobileX, mobileY, requestAccessAsync } = useMobileDeviceOrientation()
+  const [initialized, setInitialized] = useState(false)
 
-  useEffect(() => {
-    window.addEventListener('click', requestAccessAsync, {passive: true})
-
-    return () => window.removeEventListener('click', requestAccessAsync)
-  }, [requestAccessAsync])
+  const handleOverlayTap = useCallback(() => {
+    requestAccessAsync()
+    setInitialized(true)
+  }, [setInitialized, requestAccessAsync])
 
   useGsapFrame(() => {
-    // console.log({ mobileX, mobileY })
-
-    if (mobileX === undefined || mobileY === undefined) return
-
-    setAngle(mobileX)
-  }, [])
+    setOrientation({
+      x: mobileX,
+      y: mobileY,
+      normalized: {
+        x: (mobileX / 180) * 2 - 1,
+        y: -(mobileY / 90) * 2 + 1
+      }
+    })
+  })
 
   return (
     <div
       style={{
+        position: 'relative',
         width: '100vw',
         height: '100vh',
         backgroundImage: `conic-gradient(from ${
-          isDesktop ? 360 * (normalized.x + normalized.y) : angle
+          isDesktop
+            ? 360 * 0.5 * (normalized.x + normalized.y)
+            : 360 * (orientation.normalized.x + orientation.normalized.y)
         }deg at 50% 50%, #438D01 -33.51deg, #E2BF7B 147.65deg, #438D01 240deg, #438D01 326.49deg, #E2BF7B 507.65deg)`,
         backgroundRepeat: 'repeat',
         backgroundSize: '30px 30px',
         backgroundPositionX: '50%',
         backgroundPositionY: '50%'
       }}
-    ></div>
+    >
+      {!isDesktop && (
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            right: 0,
+            background: 'black'
+          }}
+        >
+          <div>x: {Math.round(orientation.x)}</div>
+          <div>y: {Math.round(orientation.y)}</div>
+          <div>normalized x: {orientation.normalized.x.toFixed(4)}</div>
+          <div>normalized y: {orientation.normalized.y.toFixed(4)}</div>
+        </div>
+      )}
+
+      {!initialized && !isDesktop && (
+        <div
+          onTouchStart={handleOverlayTap}
+          style={{
+            position: 'fixed',
+            width: '100%',
+            height: '100%',
+            zIndex: 10,
+            background: '#000000DD',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          Tap to start
+        </div>
+      )}
+    </div>
   )
 }
 
