2e1aef10444e19a74da190b03e3ca1ce736d6b7b
Matias Perez <matiasperezpc@hotmail.com>
2022-09-28 12:21:16 -0300

    karl bg

---
+++ src/experiments/29.karl-background.js
@@ -0,0 +1,100 @@
+import { Environment, useGLTF } from '@react-three/drei'
+import { useThree } from '@react-three/fiber'
+import { useLayoutEffect, useRef } from 'react'
+
+import { Loader, useLoader } from '~/components/common/loader'
+import { R3FCanvasLayout } from '~/components/layout/r3f-canvas-layout'
+
+import { DURATION, gsap } from '../lib/gsap'
+import { trackCursor } from '../lib/three'
+
+const MODEL_NAME = 'KJ_Web_Scene.glb'
+const config = {
+  scale: 0.6,
+  ambientLight: 0.1,
+  background: '#000',
+  environment: 'sunset',
+  camXPosition: -2,
+  camYPosition: 35,
+  camZPosition: 15,
+  camXRotation: -Math.PI / 2.8,
+  camYRotation: 0,
+  camZRotation: 0
+}
+
+const KarlBg = () => {
+  const cardsRef = useRef([])
+  const { camera } = useThree()
+  const setLoaded = useLoader((s) => s.setLoaded)
+  const model = useGLTF(
+    `/models/${MODEL_NAME}`,
+    undefined,
+    undefined,
+    (loader) => {
+      loader.manager.onLoad = () => setLoaded()
+    }
+  )
+
+  useLayoutEffect(() => {
+    if (!model || !camera) return
+
+    camera.position.set(
+      config.camXPosition,
+      config.camYPosition,
+      config.camZPosition
+    )
+    camera.rotation.set(
+      config.camXRotation,
+      config.camYRotation,
+      config.camZRotation
+    )
+
+    /* Cards hover */
+    const cards = model.scene.children.find((o) => o.name === 'Cards')
+    cardsRef.current = cards.children
+
+    const mouseTracker = trackCursor((cursor) => {
+      gsap.to(camera.rotation, {
+        overwrite: true,
+        duration: DURATION / 2.5,
+        x: config.camXRotation + cursor.y * (Math.PI / 10),
+        y: config.camYRotation + -cursor.x * (Math.PI / 20),
+        ease: 'power2.out'
+      })
+    })
+
+    return () => {
+      mouseTracker.destroy()
+    }
+  }, [camera, model])
+
+  // useFrame((st) => {
+  // const intersecting = raycaster.intersectObjects(cardsRef.current)
+  // intersecting[0]?.object?.scale?.set?.(2, 2, 2)
+  // })
+
+  return (
+    <>
+      {/* <fog attach="fog" args={['#17171b', 30, 40]} /> */}
+
+      <color attach="background" args={[config.background]} />
+      {/* @ts-ignore */}
+      <Environment preset={config?.environment} />
+      <ambientLight intensity={config?.ambientLight} />
+      {/* <OrbitControls /> */}
+
+      <group scale={config?.scale}>
+        {/* @ts-ignore */}
+        <primitive object={model.scene} />
+      </group>
+    </>
+  )
+}
+
+KarlBg.Layout = (props) => (
+  <R3FCanvasLayout {...props} htmlChildren={<Loader />} />
+)
+KarlBg.Title = 'Karl Background'
+KarlBg.Tags = '3d'
+
+export default KarlBg
