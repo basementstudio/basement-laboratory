9d1a47949f6b22292c7b91dba04cea91f5f49143
Matias Perez <matiasperezpc@hotmail.com>
2023-02-03 17:59:53 -0300

    upd

--- src/experiments/49.bezier-tests.js
+++ src/experiments/49.bezier-tests.js
@@ -3,27 +3,66 @@ import { useFrame } from '@react-three/fiber'
 import { useMemo, useRef } from 'react'
 import * as THREE from 'three'
 
-const cameraPositions = new THREE.CubicBezierCurve3(
-  new THREE.Vector3(-1, 0, 1),
-  new THREE.Vector3(2.0, 1.5, 0),
-  new THREE.Vector3(-0.5, 1.5, 0),
-  new THREE.Vector3(1, 3, 1)
-)
+import { useMousetrap } from '~/hooks/use-mousetrap'
+import { useToggleState } from '~/hooks/use-toggle-state'
+
+import curveComplex from '../../public/splines/curve-complex.json'
+
+/* 
+  This works like:
+  [..points, controls..]
+                        -----> CubicBezierCurve3
+  [..points, controls..]
+                        -----> CubicBezierCurve3
+  [..points, controls..]
+                        -----> CubicBezierCurve3
+  [..points, controls..]
+*/
+
+const getBezierCurves = (curve, scale = 1) => {
+  const beziers = []
+
+  for (let i = 0; i < curve.length; i += 1) {
+    const p1 = curve[i]
+    const p2 = curve[i + 1]
+
+    if (!p2) break
+
+    beziers.push(
+      new THREE.CubicBezierCurve3(
+        new THREE.Vector3(p1.px, p1.pz, p1.py).multiplyScalar(scale),
+        new THREE.Vector3(p1.hrx, p1.hrz, p1.hry).multiplyScalar(scale),
+        new THREE.Vector3(p2.hlx, p2.hlz, p2.hly).multiplyScalar(scale),
+        new THREE.Vector3(p2.px, p2.pz, p2.py).multiplyScalar(scale)
+      )
+    )
+  }
+
+  return beziers
+}
+
+const cameraPositions = getBezierCurves(curveComplex, 2)
 
 const BezierTests = () => {
+  const { handleToggle: toggleCamAttached, isOn: isCamAttached } =
+    useToggleState()
   const boxRef = useRef()
 
-  const cameraPositionsPointsPath = useMemo(() => {
+  const pointsPath = useMemo(() => {
     const pointsPath = new THREE.CurvePath()
-    pointsPath.add(cameraPositions)
+    cameraPositions.forEach((p) => pointsPath.add(p))
 
+    return pointsPath
+  }, [])
+
+  const cameraPositionsPointsPath = useMemo(() => {
     const points = pointsPath.curves.reduce(
-      (p, d) => [...p, ...d.getPoints(50)],
+      (p, d) => [...p, ...d.getPoints(40)],
       []
     )
 
     return points
-  }, [])
+  }, [pointsPath])
 
   const cameraPositionsAttrArray = useMemo(() => {
     return new Float32Array(
@@ -31,21 +70,33 @@ const BezierTests = () => {
     )
   }, [cameraPositionsPointsPath])
 
-  useFrame(() => {
+  useFrame((s) => {
     if (!boxRef.current) return
 
     const t = Math.sin(Date.now() / 1000) * 0.5 + 0.5
-    const point = cameraPositions.getPointAt(t)
+    const point = pointsPath.getPointAt(t)
 
     boxRef.current.position.copy(point)
     boxRef.current.lookAt(0, 0, 0)
+
+    if (isCamAttached) {
+      s.camera.position.copy(point)
+      s.camera.lookAt(0, 0, 0)
+    }
   }, [])
+
+  useMousetrap([
+    {
+      keys: 'a',
+      callback: toggleCamAttached
+    }
+  ])
 
   return (
     <>
       <OrbitControls />
       <axesHelper />
-      <gridHelper args={[10, 10]} />
+      <gridHelper args={[20, 20]} />
 
       <Box args={[0.5, 0.5, 0.5]} ref={boxRef}>
         <meshNormalMaterial />
@@ -64,6 +115,8 @@ const BezierTests = () => {
   )
 }
 
-BezierTests.Title = 'Bezier Tests'
+BezierTests.Title = 'Imported Bezier Curves'
+// BezierTests.Description = 'Bezier curves imported from a JSON file'
+BezierTests.Tags = 'private'
 
 export default BezierTests
