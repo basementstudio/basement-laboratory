0469d5e445703e50159f2a9f6e614665f4ea9653
Nazareno Oviedo <nazareno@basement.studio>
2023-03-20 16:54:06 -0300

    feat: improve model, hdr

--- src/experiments/54.scale-transmission-material.js
+++ src/experiments/54.scale-transmission-material.js
@@ -11,7 +11,7 @@ import { useControls } from 'leva'
 import React from 'react'
 
 const ScaleTransmissionMaterial = () => {
-  const { nodes } = useGLTF('/models/hero-scale-v2.gltf')
+  const { nodes } = useGLTF('/models/hero-scale-v3.gltf')
   const roughnessMap = useTexture('/textures/dirtness.jpg')
 
   const texture = useTexture('/images/matcap/CL-107.png')
@@ -20,13 +20,13 @@ const ScaleTransmissionMaterial = () => {
     meshPhysicalMaterial: false,
     transmissionSampler: false,
     backside: false,
-    samples: { value: 10, min: 1, max: 32, step: 1 },
-    resolution: { value: 2048, min: 256, max: 2048, step: 256 },
+    samples: { value: 7, min: 1, max: 32, step: 1 },
+    resolution: { value: 1024, min: 256, max: 2048, step: 256 },
     transmission: { value: 1, min: 0, max: 1 },
-    roughness: { value: 0.0, min: 0, max: 1, step: 0.01 },
+    roughness: { value: 1.0, min: 0, max: 1, step: 0.01 },
     thickness: { value: 1, min: 0, max: 10, step: 0.01 },
     ior: { value: 1.14, min: 1, max: 5, step: 0.01 },
-    chromaticAberration: { value: 0.04, min: 0, max: 1 },
+    chromaticAberration: { value: 1.0, min: 0, max: 1 },
     anisotropy: { value: 0.1, min: 0, max: 1, step: 0.01 },
     distortion: { value: 0.0, min: 0, max: 1, step: 0.01 },
     distortionScale: { value: 0.3, min: 0.01, max: 1, step: 0.01 },
@@ -41,10 +41,7 @@ const ScaleTransmissionMaterial = () => {
     <>
       {/* <axesHelper />
       <gridHelper /> */}
-      <Environment
-        files="https://dl.polyhaven.org/file/ph-assets/HDRIs/hdr/1k/dancing_hall_1k.hdr"
-        blur={10}
-      />
+      <Environment files="/images/studio.hdr" blur={1} />
 
       <OrbitControls />
       <PerspectiveCamera makeDefault position={[0, 0, 10]} />
@@ -88,10 +85,9 @@ const ScaleTransmissionMaterial = () => {
           <mesh
             castShadow
             receiveShadow
-            geometry={nodes.Middle1.geometry}
-            material={nodes.Middle1.material}
+            geometry={nodes.Middle2.geometry}
+            material={nodes.Middle2.material}
             position={[377.31, 407.41, -112.79]}
-            rotation={[-0.22, -0.49, 0.91]}
           >
             <MeshTransmissionMaterial {...config} roughnessMap={roughnessMap} />
           </mesh>
