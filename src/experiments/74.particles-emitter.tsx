cd93e9af8c1daecff62d146bde62e9f1681608a9
valebearzotti <valebearzotti1@gmail.com>
2024-08-16 16:27:08 -0300

    earth & cursor gravity + control value updates & improvements

--- src/experiments/74.particles-emitter.tsx
+++ src/experiments/74.particles-emitter.tsx
@@ -1,11 +1,22 @@
 import { useControls } from 'leva'
-import React, { useEffect, useMemo, useRef } from 'react'
+import type { MutableRefObject } from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import { HTMLLayout } from '~/components/layout/html-layout'
 
+export function useStateToRef<T = unknown>(initial: T): MutableRefObject<T> {
+  const ref = useRef<T>(initial)
+
+  useEffect(() => {
+    ref.current = initial
+  }, [initial])
+
+  return ref
+}
+
 function hexToRgb(hex: string) {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
-  if (!result || !result[1] || !result[2] || !result[3]) return null
+  if (!result || !result[1] || !result[2] || !result[3]) return '187, 134, 255'
   return `${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(
     result[3],
     16
@@ -30,23 +41,34 @@ function ParticlesEmitter() {
 
   /* Controls */
   const {
-    particleCreationRate,
-    particleSize,
-    particleColor,
-    gravityStrength,
-    particleLifespan,
+    creationRate,
+    size,
+    color,
+    lifespan,
     emissionRadius,
     maxParticles,
-    updateFrequency
+    earthGravity,
+    cursorGravity
   } = useControls({
-    particleCreationRate: { value: 1, min: 0.1, max: 1, step: 0.1 },
-    particleSize: { value: 4, min: 1, max: 10, step: 1 },
-    particleColor: '#FF4D00',
-    gravityStrength: { value: 0.2, min: -1, max: 1, step: 0.01 },
-    particleLifespan: { value: 40, min: 10, max: 100, step: 10 },
+    creationRate: { value: 1, min: 0.1, max: 100, step: 0.1 },
+    size: { value: 4, min: 1, max: 10, step: 1 },
+    color: '#FF4D00',
+    lifespan: { value: 40, min: 10, max: 100, step: 10 },
     emissionRadius: { value: 1, min: 1, max: 10, step: 0.1 },
-    maxParticles: { value: 60, min: 10, max: 200, step: 10 },
-    updateFrequency: { value: 1, min: 1, max: 2, step: 1 }
+    maxParticles: { value: 60, min: 10, max: 10000, step: 10 },
+    earthGravity: { value: 0.1, min: 0, max: 1, step: 0.01 },
+    cursorGravity: { value: 0.1, min: -1, max: 1, step: 0.01 }
+  })
+
+  const configRef = useStateToRef({
+    creationRate,
+    size,
+    color,
+    lifespan,
+    emissionRadius,
+    maxParticles,
+    earthGravity,
+    cursorGravity
   })
 
   /* Mouse Position */
@@ -71,86 +93,68 @@ function ParticlesEmitter() {
     }
   }, [])
 
-  const color = useMemo(
-    () => hexToRgb(particleColor) || '187, 134, 255',
-    [particleColor]
-  )
-
   /* Particles */
   useEffect(() => {
     const container = containerRef.current
     if (!container) return
 
-    let frameCount = 0
-
     const renderParticles = () => {
-      frameCount++
+      let shadow = ''
 
-      if (frameCount % updateFrequency === 0) {
-        let shadow = ''
+      particles.current = particles.current.filter((p, i) => {
+        // earth gravity
+        p.speedY += configRef.current.earthGravity
 
-        particles.current = particles.current.filter((p, i) => {
-          // gravity
-          if (gravityStrength > 0) p.speedY += gravityStrength * 0.98
+        const dx = mousePosition.current.x - p.x
+        const dy = mousePosition.current.y - p.y
+        const distance = Math.sqrt(dx * dx + dy * dy)
 
-          const dx = mousePosition.current.x - p.x
-          const dy = mousePosition.current.y - p.y
-          const distance = Math.sqrt(dx * dx + dy * dy)
+        // cursor gravity
+        const cursorGravity = configRef.current.cursorGravity
+        p.speedX += (dx / distance) * cursorGravity
+        p.speedY += (dy / distance) * cursorGravity
 
-          if (gravityStrength < 0) {
-            // push away
-            p.speedX -= (dx / distance) * updateFrequency
-            p.speedY -= (dy / distance) * updateFrequency
-          } else {
-            // pull towards mouse
-            p.speedX += (dx / distance) * updateFrequency
-            p.speedY += (dy / distance) * updateFrequency
-          }
+        // particle position
+        p.x += p.speedX
+        p.y += p.speedY
 
-          // particle position
-          p.x += p.speedX
-          p.y += p.speedY
+        p.size = Math.max(
+          p.size - configRef.current.size / configRef.current.lifespan,
+          0
+        )
+        p.opacity = Math.max(p.opacity - 1 / configRef.current.lifespan, 0)
 
-          p.size = Math.max(
-            p.size - particleSize / particleLifespan / updateFrequency,
-            0
-          )
-          p.opacity = Math.max(
-            p.opacity - 1 / particleLifespan / updateFrequency,
-            0
-          )
+        if (i > 0) shadow += ', '
+        shadow += `${p.x - p.size}px ${p.y}px 0px ${p.size}px rgba(${hexToRgb(
+          configRef.current.color
+        )}, ${p.opacity})`
 
-          if (i > 0) shadow += ', '
-          shadow += `${p.x - p.size}px ${p.y}px 0px ${
-            p.size
-          }px rgba(${color}, ${p.opacity})`
+        return p.size > 0 && p.opacity > 0
+      })
 
-          return p.size > 0 && p.opacity > 0
-        })
+      if (particles.current.length < configRef.current.maxParticles) {
+        const particlesToCreate = Math.min(
+          configRef.current.maxParticles - particles.current.length,
+          Math.ceil(configRef.current.creationRate)
+        )
 
-        if (
-          particles.current.length < maxParticles &&
-          Math.random() < particleCreationRate / updateFrequency
-        ) {
+        for (let i = 0; i < particlesToCreate; i++) {
           const angle = Math.random() * Math.PI * 2
-
-          let radius = Math.PI * Math.pow(emissionRadius, 2) * gravityStrength
-
-          if (gravityStrength < 0) radius *= -1
+          const radius = configRef.current.emissionRadius
 
           particles.current.push({
             x: mousePosition.current.x + Math.cos(angle) * radius,
             y: mousePosition.current.y + Math.sin(angle) * radius,
-            size: particleSize,
-            speedX: (Math.random() - 0.5) * 0.5 * radius,
-            speedY: (Math.random() - 0.5) * 0.5 * radius,
+            size: configRef.current.size,
+            speedX: (Math.random() - 0.5) * 2,
+            speedY: (Math.random() - 0.5) * 2,
             opacity: 1,
-            color: `rgba(${color}, 1)`
+            color: `rgba(${hexToRgb(configRef.current.color)}, 1)`
           })
         }
-
-        container.style.boxShadow = shadow
       }
+
+      container.style.boxShadow = shadow
 
       animationRef.current = requestAnimationFrame(renderParticles)
     }
@@ -162,16 +166,7 @@ function ParticlesEmitter() {
         cancelAnimationFrame(animationRef.current)
       }
     }
-  }, [
-    particleCreationRate,
-    particleSize,
-    color,
-    gravityStrength,
-    particleLifespan,
-    emissionRadius,
-    maxParticles,
-    updateFrequency
-  ])
+  }, [configRef])
 
   return (
     <div
